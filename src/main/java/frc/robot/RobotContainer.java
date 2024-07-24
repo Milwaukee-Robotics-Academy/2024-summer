949c9b3818c8986c31da472a3d22dcd56aa63e0c
ErikSLee <ErikSLee@gmail.com>
2024-07-24 17:42:18 -0500

    shooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,7 +36,7 @@ import frc.robot.subsystems.Shooter;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain m_drivetrain = new Drivetrain();
-//TODO: implement shooter  private final Shooter m_shooter = new Shooter();
+  private final Shooter m_shooter = new Shooter();
   // private final Joystick m_joystick = new Joystick(0);
   private final CommandXboxController m_driver = new CommandXboxController(0);
   // private final CommandXboxController m_operator = new
@@ -128,9 +128,9 @@ public class RobotContainer {
   private void initializeShooterControls() {
     // Connect the buttons to commands
     m_driver.start().onTrue(m_drivetrain.getInvertControlsCommand());
-    // m_driver.x().whileTrue(m_shooter.getShootWhenReadyCommands());
-    // m_driver.y().whileTrue(m_shooter.getIntakeCommand());
-    // m_driver.b().onTrue(m_shooter.getStopCommand());
+    m_driver.x().whileTrue(m_shooter.getShootWhenReadyCommands());
+    m_driver.y().whileTrue(m_shooter.getIntakeCommand());
+    m_driver.b().onTrue(m_shooter.getStopCommand());
     // m_driver.a().whileTrue(new AmpShoot(m_shooter).withTimeout(5));
     SmartDashboard.putNumber("TopShooterMotor", 100.0);
     SmartDashboard.putNumber("BottomShooterMotor", 100.0);
